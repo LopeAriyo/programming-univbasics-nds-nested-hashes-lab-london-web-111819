ba4514f368ca145a735ef5c0578228ecd7001781
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-04 20:05:39 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,7 +20,27 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 
-  aoh_two = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G] # incorrect
+  aoh_two = {
+    :DON_G => { 
+      name:  "Don Gately", 
+      occupation: "Live-in Staff" 
+    },
+    :JOELLE_VD => { 
+      name:  "Joelle van Dyne", 
+      occupation: "Radio Personality" 
+    },
+    :PAT_M => { 
+      name: "Pat Monteseian", 
+      occupation: "Staff"
+    }, 
+    :KATE_G => {
+      name:  "Kate Gompert",
+      occupation: "None"
+    }, 
+    :BRUCE_G => {
+      name:  "Bruce Green", 
+      occupation: "Fan of Mildred"
+  }}
 
   p aoh_two
 end
