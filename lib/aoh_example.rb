4af7346730c814e17b11aef4bec552bbd2f21151
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-04 20:13:49 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,24 +21,23 @@ def literal_aoh
   # held in the constants
 
   aoh_two =
-  [
-    :DON_G => {
+  [{
       :name =>  "Don Gately",
       :occupation => "Live-in Staff"
     },
-    :JOELLE_VD => {
+    {
       :name =>  "Joelle van Dyne",
       :occupation => "Radio Personality"
     },
-    :PAT_M => {
+    {
       :name => "Pat Monteseian",
       :occupation => "Staff"
     },
-    :KATE_G => {
+    {
       :name =>  "Kate Gompert",
       :occupation => "None"
     },
-    :BRUCE_G => {
+    {
       :name =>  "Bruce Green",
       :occupation => "Fan of Mildred"
   }]
