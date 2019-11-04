fb6f0fbbbe0650880a860d5758a944a444662f75
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-04 20:10:53 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,7 +20,7 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 
-  aoh_two = 
+  aoh_two =
   {
     :DON_G => {
       :name =>  "Don Gately",
